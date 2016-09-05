37ff91be112ed38a0cf4c6d4e1d27cdd5dc0e445
Jon Welker <JWelker110@gmail.com>
2016-09-05 11:06:21 -0500

    remove compiled

--- models/client.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import ndb
-
-
-class Client(ndb.Model):
-    # used for accessing Google API on behalf of the requesting app
-    client_id = ndb.StringProperty(required=True)
-    client_secret = ndb.StringProperty(required=True)
