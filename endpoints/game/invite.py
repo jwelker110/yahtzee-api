5eaefa1bc6d204d9e1f50e35830c3478c12b7852
Jon Welker <JWelker110@gmail.com>
2016-09-11 06:57:29 -0500

    Using user key instead of urlsafe string

--- endpoints/game/invite.py
+++ endpoints/game/invite.py
@@ -5,7 +5,7 @@ from models import Game, Invite, TurnCard
 from google.appengine.ext.ndb import Key
 
 
-class InviteHandler(request.RequestHandler):
+class AcceptInviteHandler(request.RequestHandler):
     @decorators.jwt_required
     def post(self, payload):
         """
@@ -21,7 +21,7 @@ class InviteHandler(request.RequestHandler):
         data = json.loads(self.request.body)
         player_two_key = data.get('player_two_key')
 
-        user = Key(urlsafe=payload.get('userKey')).get()
+        user = Key(urlsafe=payload.get('userKey'))
 
         if user is None:
             # not sure how the JWT slipped through but they aren't authorized to do this
@@ -35,14 +35,16 @@ class InviteHandler(request.RequestHandler):
 
         # awesome we have player one and two
         game = Game.query(
-            Game.player_one == payload.get('userKey'),
-            Game.player_two == player_two_key,
-            Game.completed == False).get()
+            Game.player_one == user,
+            Game.player_two == player_two.key,
+            Game.player_one_completed == False,
+            Game.player_two_completed == False).get()
 
         if game is None:
-            game = Game.query(Game.player_one == player_two_key,
-                              Game.player_two == payload.get('userKey'),
-                              Game.completed == False).get()
+            game = Game.query(Game.player_one == player_two.key,
+                              Game.player_two == user,
+                              Game.player_one_completed == False,
+                              Game.player_two_completed == False).get()
 
         # if a game exists between these users, they need to finish it first please
         if game is not None:
@@ -52,8 +54,8 @@ class InviteHandler(request.RequestHandler):
 
         # let's check for an existing invite between these players
         invite = Invite.query(
-            Invite.from_player == payload.get('userKey'),
-            Invite.to_player == player_two_key,
+            Invite.from_player == user,
+            Invite.to_player == player_two.key,
             Invite.accepted == False,
             Invite.rejected == False
         ).get()
@@ -62,8 +64,8 @@ class InviteHandler(request.RequestHandler):
             return self.response.set_status(400, "A pending invite must be accepted or declined before creating another one")
 
         invite = Invite.query(
-            Invite.from_player == player_two_key,
-            Invite.to_player == payload.get('userKey'),
+            Invite.from_player == player_two.key,
+            Invite.to_player == user,
             Invite.accepted == False,
             Invite.rejected == False
         ).get()
@@ -73,16 +75,16 @@ class InviteHandler(request.RequestHandler):
             # start a game, and create the turncards for each player
             try:
                 game = Game(
-                    player_one=player_two_key,
-                    player_two=payload.get('userKey')
+                    player_one=player_two.key,
+                    player_two=user
                 )
                 player_one_turncard = TurnCard(
-                    owner=payload.get('userKey'),
-                    game=game.key.urlsafe()
+                    owner=player_two.key,
+                    game=game.key
                 )
                 player_two_turncard = TurnCard(
-                    owner=player_two_key,
-                    game=game.key.urlsafe()
+                    owner=user,
+                    game=game.key
                 )
                 player_one_turncard.put()
                 player_two_turncard.put()
@@ -99,8 +101,8 @@ class InviteHandler(request.RequestHandler):
         # alright there are no invites between these players yet so let's make one
         try:
             invite = Invite(
-                from_player=payload.get('userKey'),
-                to_player=player_two_key
+                from_player=user,
+                to_player=player_two.key
             )
             invite.put()
             return self.response.set_status(200)
