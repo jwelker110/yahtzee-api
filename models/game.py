a8a62a4fdf78647355df8727434b40b77c31228f
Jon Welker <JWelker110@gmail.com>
2016-09-11 19:33:56 -0500

    Add player names and game complete properties

--- models/game.py
+++ models/game.py
@@ -3,14 +3,17 @@ from google.appengine.ext import ndb
 
 class Game(ndb.Model):
     player_one = ndb.KeyProperty(kind='User', required=True)
+    player_one_name = ndb.StringProperty(required=True)
     player_two = ndb.KeyProperty(kind='User', required=True)
+    player_two_name = ndb.StringProperty(required=True)
 
     date_created = ndb.DateTimeProperty(auto_now_add=True)
 
-    cancelled = ndb.BooleanProperty(default=False)
-    completed = ndb.BooleanProperty(default=False)
-
     # PLAYER ONE
+    player_one_cancelled = ndb.BooleanProperty(default=False)
+    player_one_completed = ndb.BooleanProperty(default=False)
+
+    player_one_last_turn_date = ndb.DateTimeProperty()
     player_one_ones = ndb.IntegerProperty(default=None)
     player_one_twos = ndb.IntegerProperty(default=None)
     player_one_threes = ndb.IntegerProperty(default=None)
@@ -37,6 +40,10 @@ class Game(ndb.Model):
     player_one_score_total = ndb.ComputedProperty(lambda self: self.player_one_upper_total + self.player_one_lower_total)
 
     # PLAYER TWO
+    player_two_cancelled = ndb.BooleanProperty(default=False)
+    player_two_completed = ndb.BooleanProperty(default=False)
+
+    player_two_last_turn_date = ndb.DateTimeProperty()
     player_two_ones = ndb.IntegerProperty(default=None)
     player_two_twos = ndb.IntegerProperty(default=None)
     player_two_threes = ndb.IntegerProperty(default=None)
@@ -61,3 +68,5 @@ class Game(ndb.Model):
     player_two_lower_total = ndb.IntegerProperty(default=0)
 
     player_two_score_total = ndb.ComputedProperty(lambda self: self.player_two_upper_total + self.player_two_lower_total)
+
+    game_completed = ndb.ComputedProperty(lambda self: self.player_one_completed and self.player_two_completed)
