4c717c4f0bd9cad90ad8a7860591675de450b51c
Jon Welker <JWelker110@gmail.com>
2016-09-13 05:50:12 -0500

    Add usernames

--- models/invite.py
+++ models/invite.py
@@ -3,7 +3,9 @@ from google.appengine.ext import ndb
 
 class Invite(ndb.Model):
     from_player = ndb.KeyProperty(kind='User', required=True)
+    from_player_name = ndb.StringProperty(required=True)
     to_player = ndb.KeyProperty(kind='User', required=True)
+    to_player_name = ndb.StringProperty(required=True)
 
     accepted = ndb.BooleanProperty(default=False)
     rejected = ndb.BooleanProperty(default=False)
