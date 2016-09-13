b97d8e66b88c0dc64ef73d89cc1a84467881788c
Jon Welker <JWelker110@gmail.com>
2016-09-13 05:49:02 -0500

    Update imports

--- ep/__init__.py
+++ ep/__init__.py
@@ -1,3 +1,3 @@
-from game import CreateInviteHandler, UserGamesHistoryHandler, UserRollHistoryHandler
+from game import CreateInviteHandler, UserGamesHistoryHandler, UserRollHistoryHandler, RetrieveInviteHandler
 from auth import UserHandler, ReauthHandler
 from user import UserAllHandler, UserRankHandler, HighScoreHandler
