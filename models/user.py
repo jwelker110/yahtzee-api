915367948462c10eca69d5cf33948f955c861ffb
Jon Welker <JWelker110@gmail.com>
2016-09-15 05:39:28 -0500

    Remove unused lowercase username prop

--- models/user.py
+++ models/user.py
@@ -3,7 +3,6 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):
     username = ndb.StringProperty(required=True)
-    username_lower = ndb.ComputedProperty(lambda self: self.username.lower())
     email = ndb.StringProperty(required=True)
 
     wins = ndb.IntegerProperty(default=0)
