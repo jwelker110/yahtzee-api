03ea2575d66829d03f5ececb942bbb5f39af2e88
Jon Welker <JWelker110@gmail.com>
2016-09-15 04:03:25 -0500

    Update test for no jwt provided reauth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,21 +1,30 @@
 import webapp2
 import webtest
 import json
+import time
 
 from base import GameTestCase
 from ep import ReauthHandler
 
+from models import User
+from helpers import token
+
 
 class TestCaseAuth(GameTestCase):
-    jwt_token_player_one = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0NzM4ODE1NDAsImlhdCI6MTQ3MzYyMjM0MCwidXNlcktleSI6ImFoSmtaWFotZVdGb2RIcGxaUzB4TkRJeE1EbHlIZ3NTQkZWelpYSWlGR3AzWld4clpYSXhNVEJBWjIxaGFXd3VZMjl0REEifQ.xEZ7kWXf7vyIqLrR5w95_gCIAw07xgVggTu--CryPms"
-    jwt_token_player_two = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NzM4ODE1ODYsInVzZXJLZXkiOiJhaEprWlhaLWVXRm9kSHBsWlMweE5ESXhNRGx5SUFzU0JGVnpaWElpRm1scGMyRjBiM0pwZUdKdmVFQm5iV0ZwYkM1amIyME0iLCJpYXQiOjE0NzM2MjIzODZ9.9ybTAPntQyFRlqalmHNHko-bi5dr47xnnjHvigWNGoo"
 
     def setUp(self):
         super(TestCaseAuth, self).setUp()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
         app = webapp2.WSGIApplication([
             ('/api/v1/user/reauth', ReauthHandler)
         ])
         self.testapp = webtest.TestApp(app)
+
+        user_one = User(username='Tester01', email='Tester01@email.com').put()
+        user_two = User(username='Tester02', email='Tester02@email.com').put()
+        self.jwt_token_player_one = token.encode_jwt({"userKey": user_one.urlsafe()})
+        self.jwt_token_player_two = token.encode_jwt({"userKey": user_two.urlsafe()})
 
     def tearDown(self):
         super(TestCaseAuth, self).tearDown()
@@ -25,6 +34,8 @@ class TestCaseAuth(GameTestCase):
                                  params=json.dumps({"jwt_token": self.jwt_token_player_one}),
                                  content_type='application/json')
         data = json.loads(resp.body)
-
         self.assertIn('jwt_token', data, 'JWT was not returned for player one')
-        self.assertNotEqual(self.jwt_token_player_one, data.get('jwt_token'))
+
+    def test_user_no_auth(self):
+        resp = self.testapp.post('/api/v1/user/reauth', expect_errors=True)
+        self.assertIn('400', str(resp))
