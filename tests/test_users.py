b6ff73e693ab034dfff6c8bf36eea6d0a0e77570
Jon Welker <JWelker110@gmail.com>
2016-09-17 17:42:17 -0500

    Add test for ranking based on wins and scores

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,12 @@
 import webapp2
 import webtest
 import json
+import random
 
+from models import Game
 from models import User
 from base import GameTestCase
-from ep import UserAllHandler
+from ep import UserAllHandler, HighScoreHandler, UserRankHandler
 
 
 class TestCaseUsers(GameTestCase):
@@ -14,7 +16,9 @@ class TestCaseUsers(GameTestCase):
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
         app = webapp2.WSGIApplication([
-            ('/api/v1/user/all', UserAllHandler)
+            ('/api/v1/user/all', UserAllHandler),
+            ('/api/v1/user/score', HighScoreHandler),
+            ('/api/v1/user/rank', UserRankHandler)
         ])
         self.testapp = webtest.TestApp(app)
 
@@ -41,3 +45,44 @@ class TestCaseUsers(GameTestCase):
         resp = self.testapp.get('/api/v1/user/all?offset=1')
         data = json.loads(resp.body)
         self.assertEqual(len(data), 1, 'Data did not contain one user entity')
+
+    def test_user_ranking(self):
+        """
+        Test to ensure that user rank is retrieved, based on most wins
+        """
+        for _ in xrange(0, 15):
+            User(username='Tester %s' % _, email='Tester@email.com', wins=random.randint(0, 10)).put()
+
+        resp = self.testapp.get('/api/v1/user/rank')
+        resp = json.loads(resp.body)
+        self.assertEqual(len(resp), 10, 'test')
+
+    def test_user_high_score(self):
+        """
+        Test to ensure that user rank is retrieved, based on most wins
+        """
+        users = []
+        for _ in xrange(0, 10):
+            users.append(User(username='Tester %s' % (_ + 1), email='Tester@email.com', wins=random.randint(0, 10)).put())
+
+        for _ in xrange(0, 5):
+            user_one = users[_].get()
+            user_two = users[9 - _].get()
+            game = Game(player_one=user_one.key,
+                         player_one_name=user_one.username,
+                         player_two=user_two.key,
+                         player_two_name=user_two.username,
+                         player_one_upper_sub_total=random.randint(10, 80),
+                         player_one_lower_total=random.randint(30, 150),
+                         player_two_upper_sub_total=random.randint(10, 80),
+                         player_two_lower_total=random.randint(30, 150),
+                         player_one_completed=True,
+                         player_two_completed=True)
+            game.put()
+            # if you'd like to see the scores
+            # print "player one score: %s" % game.player_one_score_total
+            # print "player two score: %s" % game.player_two_score_total
+            # print "++++++++++++++++++++++++++"
+        resp = self.testapp.get('/api/v1/user/score')
+        resp = json.loads(resp.body)
+        self.assertEqual(len(resp), 5, '5 scores were not returned')
