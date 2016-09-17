a64f4c5b179f284da94bd58c4f126bceee891399
Jon Welker <JWelker110@gmail.com>
2016-09-17 09:29:38 -0500

    Add turn handlers to import

--- ep/game/__init__.py
+++ ep/game/__init__.py
@@ -1,2 +1,3 @@
 from invite import CreateInviteHandler, RetrieveInviteHandler, CancelInviteHandler
 from history import UserGamesHistoryHandler, UserRollHistoryHandler
+from turn import TakeTurnHandler, NewTurnHandler, CompleteTurnHandler
