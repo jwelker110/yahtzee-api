5c784228d932cbe333ba6654197d7620ef0756dd
Jon Welker <JWelker110@gmail.com>
2016-09-18 20:01:45 -0500

    Remove handlers that are used as endpoints now

--- main.py
+++ main.py
@@ -16,28 +16,21 @@
 #
 import webapp2
 
-from ep import CompleteTurnHandler, NewTurnHandler, TakeTurnHandler, \
-    CreateInviteHandler, RetrieveInviteHandler, CancelInviteHandler, \
-    UserAllHandler, UserGamesHandler, UserRollHistoryHandler, UserGamesHistoryHandler, \
-    ViewGameHandler, UserRankHandler, HighScoreHandler, CancelGameHandler, ReauthHandler, \
-    UserHandler
-
-
 app = webapp2.WSGIApplication([
-    ('/api/v1/turn/complete', CompleteTurnHandler),
-    ('/api/v1/turn/new', NewTurnHandler),
-    ('/api/v1/turn/take', TakeTurnHandler),
-    ('/api/v1/invite/create', CreateInviteHandler),
-    ('/api/v1/invite/retrieve', RetrieveInviteHandler),
-    ('/api/v1/invite/cancel', CancelInviteHandler),
-    ('/api/v1/game/view', ViewGameHandler),
-    ('/api/v1/game/current', UserGamesHandler),
-    ('/api/v1/game/forfeit', CancelGameHandler),
-    ('/api/v1/game/rolls', UserRollHistoryHandler),
-    ('/api/v1/game/history', UserGamesHistoryHandler),
-    ('/api/v1/auth/reauth', ReauthHandler),
-    ('/api/v1/auth/user', UserHandler),
-    ('/api/v1/user/all', UserAllHandler),
-    ('/api/v1/user/rank', UserRankHandler),
-    ('/api/v1/user/highscore', HighScoreHandler),
+    # ('/api/v1/turn/complete', CompleteTurnHandler),
+    # ('/api/v1/turn/new', NewTurnHandler),
+    # ('/api/v1/turn/take', TakeTurnHandler),
+    # ('/api/v1/invite/create', CreateInviteHandler),
+    # ('/api/v1/invite/retrieve', RetrieveInviteHandler),
+    # ('/api/v1/invite/cancel', CancelInviteHandler),
+    # ('/api/v1/game/view', ViewGameHandler),
+    # ('/api/v1/game/current', UserGamesHandler),
+    # ('/api/v1/game/forfeit', CancelGameHandler),
+    # ('/api/v1/game/rolls', UserRollHistoryHandler),
+    # ('/api/v1/game/history', UserGamesHistoryHandler),
+    # ('/api/v1/auth/reauth', ReauthHandler),
+    # ('/api/v1/auth/user', UserHandler),
+    # ('/api/v1/user/all', UserAllHandler),
+    # ('/api/v1/user/rank', UserRankHandler),
+    # ('/api/v1/user/highscore', HighScoreHandler),
 ])
